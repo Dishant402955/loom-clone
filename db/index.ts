d27e6dfe3645faccb26823ed642a3a5954ae827d
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-07-23 11:55:58 +0530

    setup postgres with drizzle

---
+++ db/index.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+
+// import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+export const db = drizzle(process.env.DATABASE_URL);
